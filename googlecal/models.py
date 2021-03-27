cc4cad7fc0a8bb2d2f511c306cc6e49007ee697b
beahoop <beahoopdesign@gmail.com>
2021-03-27 12:05:09 -0400

    view is set up to post to google calendar

--- googlecal/models.py
+++ googlecal/models.py
@@ -1,18 +1,17 @@
 from django.db import models
 from django.db import models
-from phone_field import PhoneField
 
 # Create your models here.
 
 class Event(models.Model):
     #locations
-    summary = models.CharField(max_length=100)
-    location = models.CharField(max_length=300)
+    summary = models.CharField(max_length=100, blank=True, null=True)
+    location = models.CharField(max_length=300, blank=True, null=True)
     description = models.CharField(max_length=300, blank=True, null=True)
     # start = models.JSONField(encoder=None, decoder=None, null=True)
     dateTime_start = models.DateTimeField(null=True)
     dateTime_end = models.DateTimeField(null=True)
-    # timeZone =
+    timeZone = models.CharField(max_length=100, blank=True, null=True)
     #   #
       # 'start': {
         # 'dateTime': '2021-04-05T07:00:00',
@@ -23,7 +22,9 @@ class Event(models.Model):
       #   'dateTime': '2021-04-05T09:00:00',
       #   'timeZone': 'America/Los_Angeles',
       # },
-    attendees = models.JSONField(encoder=None, decoder=None, null=True)
+    attendee_name = models.CharField(max_length=100, null=True, blank=True)
+    attendee_comment = models.CharField(max_length=100, null=True, blank=True)
+    attendee_email = models.CharField(max_length=100, null=True, blank=True)
       # 'attendees': [
       #   {
       #         'displayName' : 'Bob',
--- googlecal/urls.py
+++ googlecal/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
-from . import second
+from . import views
 from django.urls import path
 
 urlpatterns = [
     # path('cal/', views.build_service),
     # path('cal/', quickstart.main),
-    path('create/',  second.create_event),
+    path('create/',  views.create_event),
+    path('', views.ListClients.as_view())
 ]
