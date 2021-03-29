d8be22aff1dd046184190397d37b8b7ce07dc0bd
beahoop <beahoopdesign@gmail.com>
2021-03-29 11:08:25 -0400

    uploading appointments

--- clients/tests.py
+++ clients/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
-# Create your tests here.
+    # client = Client()
+from django.test import Client
+c = Client()
+response = c.post('/login/', {'username': 'bob', 'password': 'safepass1'})
+response.status_code
+
+# def test_create(self):
+#     """Test the url for "create"
+#     """
+#
+#     response = self.client.get('match/')
+#     self.assertEqual(response.status_code, 200)
--- conf/settings.py
+++ conf/settings.py
@@ -123,8 +123,6 @@ else:
         }
     }
 
-
-
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
@@ -149,7 +147,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/New_York'
 
 USE_I18N = True
 
